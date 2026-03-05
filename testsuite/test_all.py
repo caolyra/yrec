1f404178ba28c801d6fb079f856c31ff4adbb86b
M.Rendina <25330458+rendinam@users.noreply.github.com>
2026-03-05 16:40:47 -0500

    Use fractional floating point tolerance for output comparison; Allow for suppression of program output upon test failure

--- testsuite/test_all.py
+++ testsuite/test_all.py
@@ -20,9 +20,10 @@ config = cfp.ConfigParser()
 config.read("pytest.ini")
 yrec_exe = os.path.abspath( config['paths']['yrec'] )
 tests = [x for x in config['paths']['tests'].split('\n') if len(x) > 0 ]
-float_abs_tol = float(config['tolerances']['float_abs_tol'])
+float_frac_tol = float(config['tolerances']['float_frac_tol'])
 int_abs_tol = int(config['tolerances']['int_abs_tol'])
 ref_dir = config['paths']['reference_dir']
+show_output = config.getboolean('options','show_output_upon_failure')
 
 if not os.path.exists(yrec_exe):
     raise FileNotFoundError(f"YREC executable {yrec_exe} not found!")
@@ -113,9 +114,18 @@ def filevals_differ(ref_file, out_file, float_tol, int_tol):
                 except IndexError as ex:
                     difference = True
                     break
-                if type(val) == float and diff > float_tol:
-                    locs.append(i)
-                    difference = True
+
+                # Check against fractional floating point tolerance
+                if type(val) == float:
+                    if val != 0:
+                        if abs(diff/val) > float_tol:
+                            locs.append(i)
+                            difference = True
+                    else:
+                        if diff != 0:
+                            locs.append(i)
+                            difference = True
+
                 if type(val) == int and diff > int_tol:
                     locs.append(i)
                     difference = True
@@ -205,9 +215,10 @@ def test_yrec(tdir, nml1, nml2):
     os.chdir(startdir)
     out = proc.stdout.decode()
     err = proc.stderr.decode()
+    if show_output:
+        print(out)
+        print(err)
 
-    print(out)
-    print(err)
     # Extract output directory from terminal output
     outdir = None
     for line in out.split('\n'):
@@ -244,7 +255,7 @@ def test_yrec(tdir, nml1, nml2):
             shutil.copyfile(out, ref)
             print(f"{out} copied to reference")
         print(f"--- Comparing standard to {out}\n")
-        if filevals_differ(ref, out, float_abs_tol, int_abs_tol):
+        if filevals_differ(ref, out, float_frac_tol, int_abs_tol):
             outputs_identical = False
     assert outputs_identical, "Output differs from reference standard"
 
